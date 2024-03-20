d1aabe9b877c53bac8a1fb7f53ce13e2514aae25
Wagner <wagner_silvajr@hotmail.com>
2024-03-20 18:47:17 -0300

    feat: created gitignote

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1e8360968469b5b323ef488ee341f0d169a240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92bcc50f5c6397cf4c6c05ef2acee4b52509285b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
